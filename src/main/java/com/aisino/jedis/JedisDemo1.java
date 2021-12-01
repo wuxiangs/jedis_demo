8e82f977d00a0d853e8b25ce6e65d8b13bc92afb
wuxiang <1325668250@qq.com>
2021-12-01 14:25:55 +0800

    提交

---
+++ src/main/java/com/aisino/jedis/JedisDemo1.java
@@ -0,0 +1,17 @@
+package com.aisino.jedis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * @author wuxiang
+ * @date 2021/12/1 2:13 下午
+ */
+public class JedisDemo1 {
+
+    public static void main(String[] args) {
+        //创建jedis对象
+        Jedis jedis=new Jedis("localhost",6379);
+        String ping = jedis.ping();
+        System.out.println(ping);
+    }
+}
